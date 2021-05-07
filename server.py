7717b1b2c5c94757cc07bfe7c047ee22c023a7ba
Alex <alexandre.falher.pro@gmail.com>
2021-05-07 12:29:12 +0200

    wrapp flask app in class, allowing dependency injection for better testing, all previously tests passing are still passing

--- server.py
+++ server.py
@@ -2,61 +2,75 @@ import json
 from flask import Flask,render_template,request,redirect,flash,url_for
 
 
-def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+class FlaskWrapper():
+    app = Flask(__name__)
+    app.secret_key = 'something_special'
+    competitions = None
+    clubs = None
+
+    def __init__(self, competitions_path = "competitions.json", clubs_path = "clubs.json"):
+        FlaskWrapper.competitions = FlaskWrapper.loadCompetitions(competitions_path)
+        FlaskWrapper.clubs = FlaskWrapper.loadClubs(clubs_path)
+
+    @staticmethod
+    def loadClubs(path: str):
+        with open(path) as c:
+            listOfClubs = json.load(c)['clubs']
+            return listOfClubs
+
+    @staticmethod
+    def loadCompetitions(path: str):
+        with open(path) as comps:
+            listOfCompetitions = json.load(comps)['competitions']
+            return listOfCompetitions
+
+    @app.route('/')
+    def index():
+        return render_template('login.html')
+
+    @app.route('/showSummary',methods=['POST'])
+    def showSummary():
+        retrieved_clubs = [club for club in FlaskWrapper.clubs if club['email'] == request.form['email']]
+        if len(retrieved_clubs) != 0:
+            return render_template('welcome.html',club=retrieved_clubs[0],competitions=FlaskWrapper.competitions)
+        else:
+            return render_template('login.html', errors=["Email not found."])
 
 
-def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+    @app.route('/book/<competition>/<club>')
+    def book(competition,club):
+        foundClub = [c for c in FlaskWrapper.clubs if c['name'] == club]
+        foundCompetition = [c for c in FlaskWrapper.competitions if c['name'] == competition]
+        if len(foundClub) != 0 and len(foundCompetition) != 0:
+            return render_template('booking.html',club=foundClub[0],competition=foundCompetition[0])
+        else:
+            # flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=FlaskWrapper.competitions, errors=["Club or competition not found."])
 
 
-app = Flask(__name__)
-app.secret_key = 'something_special'
-
-competitions = loadCompetitions()
-clubs = loadClubs()
-
-@app.route('/')
-def index():
-    return render_template('login.html')
-
-@app.route('/showSummary',methods=['POST'])
-def showSummary():
-    retrieved_clubs = [club for club in clubs if club['email'] == request.form['email']]
-    if len(retrieved_clubs) != 0:
-        return render_template('welcome.html',club=retrieved_clubs[0],competitions=competitions)
-    else:
-        return render_template('login.html', errors=["Email not found."])
+    @app.route('/purchasePlaces',methods=['POST'])
+    def purchasePlaces():
+        competition = [c for c in FlaskWrapper.competitions if c['name'] == request.form['competition']][0]
+        club = [c for c in FlaskWrapper.clubs if c['name'] == request.form['club']][0]
+        places_required = int(request.form['places'])
+        if places_required > int(club["points"]):
+            return render_template(url_for("book"), competition=competition, club=club, errors=["Club has not enough points."])
+        if places_required > int(competition["numberOfPlaces"]):
+            return render_template(url_for("book"), competition=competition, club=club, errors=["Competition has not enough places."])
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
+        club["points"] = int(club["points"]) - places_required
+        flash('Great-booking complete!')
+        return render_template('welcome.html', club=club, competitions=FlaskWrapper.competitions)
 
 
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club]
-    foundCompetition = [c for c in competitions if c['name'] == competition]
-    if len(foundClub) != 0 and len(foundCompetition) != 0:
-        return render_template('booking.html',club=foundClub[0],competition=foundCompetition[0])
-    else:
-        # flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions, errors=["Club or competition not found."])
+    # TODO: Add route for points display
 
 
-@app.route('/purchasePlaces',methods=['POST'])
-def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    @app.route('/logout')
+    def logout():
+        return redirect(url_for('index'))
 
 
-# TODO: Add route for points display
-
-
-@app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
+if __name__ != "__main__":
+    flask_wrapper = FlaskWrapper()
+    app = flask_wrapper.app
--- tests/test_all.py
+++ tests/test_all.py
@@ -5,40 +5,39 @@ from flask.wrappers import Response
 
 import pytest
 
-from ..server import app, loadClubs, loadCompetitions
+from ..server import FlaskWrapper
 
 
 class Test_PythonTesting():
 
     def setup_class(self):
-        self.clubs = json.loads(clubs)
-        self.competitions = json.loads(competitions)
+        self.clubs = Test_PythonTesting.loadClubs()
+        self.competitions = Test_PythonTesting.loadCompetitions()
+        self.flask_wrapper = FlaskWrapper()
+        self.app = self.flask_wrapper.app
 
     def teardown_class(self):
         pass
 
     def test_loadClubs(self):
-        clubs = loadClubs()
-        assert clubs == self.clubs
+        assert self.flask_wrapper.clubs == self.clubs
 
     def test_loadCompetitions(self):
-        competitions = loadCompetitions()
-        assert competitions == self.competitions
+        assert self.flask_wrapper.competitions == self.competitions
 
     def test_index__(self):
-        with app.test_client() as client:
-            with app.app_context() as context:
-                client: FlaskClient[Response]
-                response: Response = client.get("/")
-                assert response.status_code == 200
-                assert b"<title>GUDLFT Registration</title>" in response.data
+        with self.app.test_client() as client:
+            client: FlaskClient[Response]
+            response: Response = client.get("/")
+            assert response.status_code == 200
+            assert b"<title>GUDLFT Registration</title>" in response.data
 
     @pytest.mark.parametrize("email, result, page_title, errors", [
     ("admin@irontemple.com", 200, "<title>Summary | GUDLFT Registration</title>", []),
     ("something@mail.com", 200, "<title>GUDLFT Registration</title>", ["Email not found."])
     ])
     def test_showSummary(self, email, result, page_title, errors):
-        with app.test_client() as client:
+        with self.app.test_client() as client:
             client: FlaskClient[Response]
             response: Response = client.post("/showSummary", data={
                 "email": email
@@ -55,20 +54,20 @@ class Test_PythonTesting():
         ("none", "none", 200, "<title>Summary | GUDLFT Registration</title>", ["Club or competition not found."])
     ])
     def test_book(self, competition, club, result, page_title, errors):
-        with app.test_client() as client:
+        with self.app.test_client() as client:
             client: FlaskClient[Response]
             response: Response = client.get("/book/"+ competition + "/" + club)
             assert response.status_code == result
             assert str.encode(page_title) in response.data
             assert all([str.encode(error) in response.data for error in errors])
 
-    @pytest.mark.parametrize("competition, club, places_required, result, page_title, errors", [
-        ("Spring Festival", "Iron Temple", 4, 200, "<title>Summary | GUDLFT Registration</title>", []),
-        ("Fall Classic", "Simply Lifts", 5, 200, "<title>Summary | GUDLFT Registration</title>", ["Club has not enouhg points."]),
-        ("New Horizons", "She Lifts", 4, 200, "<title>Summary | GUDLFT Registration</title>", ["Competition has not enough places."]),
+    @pytest.mark.parametrize("competition, club, places_required, result, page_title, errors, points", [
+        ("Spring Festival", "Iron Temple", 4, 200, "<title>Summary | GUDLFT Registration</title>", [], "Points available: 0"),
+        ("Fall Classic", "Simply Lift", 5, 200, "<title>Summary | GUDLFT Registration</title>", ["Club has not enough points."], ""),
+        ("New Horizons", "She Lifts", 4, 200, "<title>Summary | GUDLFT Registration</title>", ["Competition has not enough places."], ""),
     ])
-    def test_purchasePlaces(self, competition, club, places_required, result, page_title, errors):
-        with app.test_client() as client:
+    def test_purchasePlaces(self, competition, club, places_required, result, page_title, errors, points):
+        with self.app.test_client() as client:
             client: FlaskClient[Response]
             response: Response = client.post("/purchasePlaces", data={
                 "competition": competition,
@@ -78,45 +77,21 @@ class Test_PythonTesting():
             assert response.status_code == result
             assert str.encode(page_title) in response.data
             assert all([str.encode(error) in response.data for error in errors])
+            if len(errors) == 0:
+                assert str.encode(points) in response.data
 
     def test_logout(self):
-        with app.test_client() as client:
+        with self.app.test_client() as client:
             client: FlaskClient[Response]
             response: Response = client.get("/logout")
             assert response.status_code == 302
+    
+    def loadClubs():
+        with open("clubs.json") as c:
+            listOfClubs = json.load(c)['clubs']
+            return listOfClubs
 
-
-clubs = """[
-    {
-        "name":"Simply Lift",
-        "email":"john@simplylift.co",
-        "points":"4"
-    },
-    {
-        "name":"Iron Temple",
-        "email": "admin@irontemple.com",
-        "points":"4"
-    },
-    {   "name":"She Lifts",
-        "email": "kate@shelifts.co.uk",
-        "points":"12"
-    }
-]"""
-
-competitions = """[
-    {
-        "name": "Spring Festival",
-        "date": "2020-03-27 10:00:00",
-        "numberOfPlaces": "25"
-    },
-    {
-        "name": "Fall Classic",
-        "date": "2020-10-22 13:30:00",
-        "numberOfPlaces": "13"
-    },
-    {
-        "name": "New Horizons",
-        "date": "2020-10-22 13:30:00",
-        "numberOfPlaces": "3"
-    }
-]"""
+    def loadCompetitions():
+        with open("competitions.json") as comps:
+            listOfCompetitions = json.load(comps)['competitions']
+            return listOfCompetitions
