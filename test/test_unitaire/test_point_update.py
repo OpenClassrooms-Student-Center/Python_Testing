367d19b61208ea4a5cc3ef4068a8242ccaf04b8f
Mathieu <dupycournau@gmail.com>
2021-11-15 18:07:04 +0100

    add test for update point

---
+++ test/test_unitaire/test_point_update.py
@@ -0,0 +1,21 @@
+import server
+import pytest
+
+@pytest.fixture
+def client():
+    server.app.config['TESTING'] = True
+    clients = server.app.test_client()
+    return clients
+
+def test_club_point_update(client):
+    club = server.clubs[0]
+    before_points = int(club['points'])
+    competition = server.competitions[0]['name']
+    response = client.post('/purchasePlaces',
+                           data={
+                               'club': club['name'],
+                               'competition': competition,
+                               'places': 3
+                           })
+    assert response.status_code == 200
+    assert before_points != club['points']
