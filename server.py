6f61f248196fcba68e24eb4242817f88b827a65a
Lucile Garrigoux <lgarrigoux@hotmail.com>
2021-12-02 23:06:25 +0100

    showSummary bug fixed.

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 import json
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, session
 
 
 def load_clubs():
     with open('clubs.json') as c:
          list_of_clubs = json.load(c)['clubs']
          return list_of_clubs
-
 
 def load_competitions():
     with open('competitions.json') as comps:
@@ -20,23 +19,47 @@ app.secret_key = 'something_special'
 competitions = load_competitions()
 clubs = load_clubs()
 
+
 @app.route('/')
 def index():
+    """Shows the form whose data will inform show_summary()"""
     return render_template('index.html')
 
-@app.route('/showSummary',methods=['POST', 'GET'])
+@app.route('/showSummary', methods=['POST'])
 def show_summary():
-    """Show the profile of the user's club."""
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html', club=club, competitions=competitions)
+    """Check whether the entered email exists.
+    If so, store the user's email in a new cession (barring user ids).
+    If not, redirect the user to the index page.
+    """
+    if request.method == "POST":
+        email = request.form["email"]
+        error = None
+        try:
+            user = [club for club in clubs if club['email'] == request.form['email']][0]
+        except:
+            error = "Identifiants incorrects ou inexistants."
+
+        if error is None:
+            session.clear()
+            session["user_id"] = user["email"]
+            club = [club for club in clubs if club['email'] == request.form['email']][0]
+            return render_template('welcome.html',club=club,competitions=competitions)
+
+        flash(error)
+        return redirect(url_for('index'))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition, club):
+def book(competition,club):
     found_club = [c for c in clubs if c['name'] == club][0]
     found_competition = [c for c in competitions if c['name'] == competition][0]
     if found_club and found_competition:
-        return render_template('booking.html', club=found_club, competition=found_competition)
+        return render_template('booking.html',club=found_club, competition=found_competition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
@@ -47,14 +70,9 @@ def purchase_places():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     places_required = int(request.form['places'])
-    competition['number_of_places'] = int(competition['number_of_places'])-places_required
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - places_required
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
-
-
-@app.route('/logout')
-def logout():
-    return redirect(url_for('index'))
