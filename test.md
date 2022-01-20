407f7a6390f4311a76c4342e5e62c78cb182c718
AatroXiss <antoine.beaudesson@gmail.com>
2022-01-20 11:49:32 +0100

    v.0.0.3 update on unit tests

--- test.md
+++ test.md
@@ -57,31 +57,31 @@ we can make sure the competition list is correct by checking the lenght of the r
 ### book()
 
 Make a test that checks if the template is correctly loaded.
-we can make sure the template is loaded by checking if the <title> of the page is correct.
+we can make sure the template is loaded by checking if the <title> of the page is correct. [DONE]
 
 Make a test that checks if the page is correctly loaded (status_code_200).
-we can make sure the page is loaded by checking if the status code is 200.
+we can make sure the page is loaded by checking if the status code is 200. [DONE]
 
 Make a  **sad path** test that check if the user trie to book more than the available tickets in the competition. The expected result is an error message.
-we can make sure the user cannot book more than the available tickets by checking if the error message is displayed when current_ticket + booked_ticket > available_tickets.
+we can make sure the user cannot book more than the available tickets by checking if the error message is displayed when current_ticket + booked_ticket > available_tickets. [DONE]
 
 Make a **sad path** test that check if the user tries to book more than 12 tickets. The expected result is an error message.
-we can make sure the user cannot book more than 12 tickets by checking if an error message is displayed when 13 tickets are booked.
+we can make sure the user cannot book more than 12 tickets by checking if an error message is displayed when 13 tickets are booked. [DONE]
 
 Make a **happy path** test that check if the user has successfully booked a ticket.
-We can make sure the user has successfully booked a ticket by checking if the user is redirected to the summary page (status_code_200) &  if the message is correct.
+We can make sure the user has successfully booked a ticket by checking if the user is redirected to the summary page (status_code_200) &  if the message is correct. [DONE]
 
 Make a **sad path** test where the user tries to book a ticket but he has not enough points.
-we can make sure the user cannot book a ticket by checking if the user is redirected to the summary page (status_code_200) &  if an error message is displayed.
+we can make sure the user cannot book a ticket by checking if the user is redirected to the summary page (status_code_200) &  if an error message is displayed. [DONE]
 
 Make a **sad path** test where the user tries to book a ticket for a competition in the past.
-we can make sure the user cannot book a ticket by checking if the user is redirected to the summary page (status_code_200) &  if an error message is displayed.
+we can make sure the user cannot book a ticket by checking if the user is redirected to the summary page (status_code_200) &  if an error message is displayed. [DONE]
 
 
 ### purchasePlaces()
 
 Make a **happy path** test that check if the user has successfully booked a ticket and the points are deducted.
-We can make sure the user has successfully booked a ticket by checking if the user is redirected to the summary page (status_code_200) &  if the message is correct & if the points are deducted from the user's points.
+We can make sure the user has successfully booked a ticket by checking if the user is redirected to the summary page (status_code_200) &  if the message is correct & if the points are deducted from the user's points. [DONE]
 
 Make a **happy path** test that check if the user has successfully booked a ticket and the number of tickets is updated.
 We can make sure the user has successfully booked a ticket by checking if the user is redirected to the summary page (status_code_200) &  if the message is correct & if the number of tickets is updated.
--- tests/unit_test/unit_test.py
+++ tests/unit_test/unit_test.py
@@ -1,6 +1,6 @@
 # unit_test.py
 # created 19/01/2021 at 16:24 by Antoine 'AatroXiss' BEAUDESSON
-# last modified 19/01/2021 at 16:24 by Antoine 'AatroXiss' BEAUDESSON
+# last modified 20/01/2021 at 11:49 by Antoine 'AatroXiss' BEAUDESSON
 
 """ unit_test.py
 
@@ -13,7 +13,7 @@ __author__ = "Antoine 'AatroXiss' BEAUDESSON"
 __copyright__ = "Copyright 2021, Antoine 'AatroXiss' BEAUDESSON"
 __credits__ = ["Antoine 'AatroXiss' BEAUDESSON"]
 __license__ = ""
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __maintainer__ = "Antoine 'AatroXiss' BEAUDESSON"
 __email__ = "antoine.beaudesson@gmail.com"
 __status__ = "Development"
@@ -150,11 +150,108 @@ class TestShowSummary():
 
 
 class TestBook():
-    pass
+
+    def test_status_code_200_template(self, client):
+        """
+        Test that the page is loaded correctly
+        we know that because the page has the status code 200.
+        We also know that the page has the right template because
+        the page has the title booking for"""
+
+        competition_name = server.loadCompetitions()[0]['name']
+        club_name = server.loadClubs()[0]['name']
+        response = client.get('/book/' + competition_name + '/' + club_name)
+        assert response.status_code == 200
+        assert ("Booking for " + competition_name) in response.data.decode()
+
+    def test_sp_book_too_many_places(self, client):
+        """
+        Test that the user cannot book more than 12 places
+        we can now that because the page contains the error message
+        Error: you cannot book more than 12 places
+        """
+        competition_name = server.loadCompetitions()[0]['name']
+        club_name = server.loadClubs()[0]['name']
+
+        response = client.post(
+            '/purchasePlaces',
+            data={
+                'places': '13',
+                'competition': competition_name,
+                'club': club_name
+            }
+        )
+        assert response.status_code == 200
+        assert ("Error: you cannot book more than 12 places") in response.data.decode()  # noqa
+
+    def test_sp_book_no_places_available(self, client):
+        """
+        Test  that a user cannot book more than the number
+        of places available in the competition.
+        We know that the system has correctly prevented the user
+        from booking more places than the number of places available
+        because the page contains the error message
+        Error: there are no places available
+        """
+
+        competition_name = server.loadCompetitions()[0]['name']
+        club_name = server.loadClubs()[0]['name']
+
+        response = client.post(
+            '/purchasePlaces',
+            data={
+                'places': '13',
+                'competition': competition_name,
+                'club': club_name
+            }
+        )
+        assert response.status_code == 200
+        assert ("Error: there are no places available") in response.data.decode()  # noqa
+
+    def test_sp_book_no_enough_points(self, client):
+        """
+        Test that makes sure that a user cannot book a competition
+        if they do not have enough points.
+        We know that the user cannot book a competition because
+        the page contains the error message
+        Error: you do not have enough points
+        """
+
+        competition_name = server.loadCompetitions()[0]['name']
+        club_name = server.loadClubs()[0]['name']
+        club_points = int(server.loadClubs()[0]['points']) + 1
+
+        response = client.post(
+            '/purchasePlaces',
+            data={
+                'places': club_points,
+                'competition': competition_name,
+                'club': club_name
+            }
+        )
+        assert response.status_code == 200
+        assert ("Error: you do not have enough points") in response.data.decode()  # noqa
 
 
 class TestPurchasePlaces():
-    pass
+
+    def test_hp_book_succeeded_points_are_deducted(self, client):
+        competition_name = server.loadCompetitions()[0]['name']
+        club_name = server.loadClubs()[0]['name']
+        club_points = int(server.loadClubs()[0]['points'])
+
+        response = client.post(
+            '/purchasePlaces',
+            data={
+                'places': club_points,
+                'competition': competition_name,
+                'club': club_name
+            }
+        )
+        club_points_left = int(server.loadClubs()[0]['points'])
+        assert response.status_code == 200
+        assert ("Great-booking complete!") in response.data.decode()
+        assert (club_points_left == 0)
 
 
 class TestLogout():
