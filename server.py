ffffa01d3410bc94802d27b52101c49a389d3e71
flo <f@linguenheld.fr>
2022-11-05 14:18:20 +0100

    Add save(), rewrite load() and use them instead of variables to easily mock

--- server.py
+++ server.py
@@ -2,24 +2,19 @@ import json
 from flask import Flask, render_template, request, redirect, flash, url_for
 
 
-def loadClubs():
-    with open('database/clubs.json') as c:
-        listOfClubs = json.load(c)['clubs']
-        return listOfClubs
+def load_json(file_name):
+    with open(f'database/{file_name}.json') as file:
+        return json.load(file)[file_name]
 
 
-def loadCompetitions():
-    with open('database/competitions.json') as comps:
-        listOfCompetitions = json.load(comps)['competitions']
-        return listOfCompetitions
+def save_json(file_name, data):
+    with open(f'database/{file_name}.json', 'w') as file:
+        json.dump({file_name: data}, file)
 
 
 def create_app(config):
     app = Flask(__name__)
     app.secret_key = 'something_special'
-
-    competitions = loadCompetitions()
-    clubs = loadClubs()
 
     @app.route('/')
     def index():
@@ -27,27 +22,27 @@ def create_app(config):
 
     @app.route('/showSummary', methods=['POST'])
     def showSummary():
-        club = [club for club in clubs if club['email'] == request.form['email']][0]
-        return render_template('welcome.html', club=club, competitions=competitions)
+        club = [club for club in load_json('clubs') if club['email'] == request.form['email']][0]
+        return render_template('welcome.html', club=club, competitions=load_json('competitions'))
 
     @app.route('/book/<competition>/<club>')
     def book(competition, club):
-        foundClub = [c for c in clubs if c['name'] == club][0]
-        foundCompetition = [c for c in competitions if c['name'] == competition][0]
+        foundClub = [c for c in load_json('clubs') if c['name'] == club][0]
+        foundCompetition = [c for c in load_json('competitions') if c['name'] == competition][0]
         if foundClub and foundCompetition:
             return render_template('booking.html', club=foundClub, competition=foundCompetition)
         else:
             flash("Something went wrong-please try again")
-            return render_template('welcome.html', club=club, competitions=competitions)
+            return render_template('welcome.html', club=club, competitions=load_json('competitions'))
 
     @app.route('/purchasePlaces', methods=['POST'])
     def purchasePlaces():
-        competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-        club = [c for c in clubs if c['name'] == request.form['club']][0]
+        competition = [c for c in load_json('competitions') if c['name'] == request.form['competition']][0]
+        club = [c for c in load_json('clubs') if c['name'] == request.form['club']][0]
         placesRequired = int(request.form['places'])
         competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
         flash('Great-booking complete!')
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template('welcome.html', club=club, competitions=load_json('competitions'))
 
     # TODO: Add route for points display
 
