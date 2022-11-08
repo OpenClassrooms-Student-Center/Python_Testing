db338c4c1a00caea3806968eca927758ad88e950
flo <f@linguenheld.fr>
2022-11-08 22:05:37 +0100

    Add methods to mock save_json and load_json

--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -1,55 +1,72 @@
+import json
 import server
 
 
 class MockedJson():
 
-    CLUBS = 'test_clubs'
-    COMPETITIONS = 'test_competitions'
+    @staticmethod
+    def load_mocked_json(file_name):
+        """ Used to mock load_json() by returning a false list with one dict (a club or a competition) """
 
-    def load_json(self, file_name):
-
-        if file_name == MockedJson.CLUBS:
+        if 'clubs' in file_name:
             return [{"name": "name_test_club",
                      "email": "test@mail.com",
                      "points": "22"}]
 
-        elif file_name == MockedJson.COMPETITIONS:
+        elif 'competitions' in file_name:
             return [{"name": "name_test_competition",
                      "date": "2020-03-27 10:00:00",
                      "numberOfPlaces": "41"}]
-
         else:
             return []
 
-    def generate_a_new_test_file(self, file_name, monkeypatch):
-        """ Use the mocked function 'load_json()' and save datas into a real file named '<test_file_name>.json'
-            Then deactivate the monkeypatch """
-        monkeypatch.setattr('server.load_json', self.load_json)  # >>
+    @staticmethod
+    def load_test_json(file_name):
+        """ Copy/past of server.load_json to modify 'file_name' by 'test_file_name' """
 
-        mocked_datas = server.load_json(file_name)
-        server.save_json(file_name, mocked_datas)
+        file_name = f"test_{file_name}"
+        with open(f'database/{file_name}.json') as file:
+            return json.load(file)[file_name]
 
-        monkeypatch.undo()  # <<
+    @staticmethod
+    def save_test_json(file_name, data):
+        """ Copy/past of server.save_json to modify 'file_name' by 'test_file_name' """
+
+        file_name = f"test_{file_name}"
+        with open(f'database/{file_name}.json', 'w') as file:
+            json.dump({file_name: data}, file)
+
+    @staticmethod
+    def generate_a_new_test_file(file_name, monkeypatch):
+        """ Use 'load_mocked_json()' and save data
+            into a real file named '<test_file_name>.json' """
+
+        mocked_data = MockedJson.load_mocked_json(file_name)
+        MockedJson.save_test_json(file_name, mocked_data)
+
+    @staticmethod
+    def monkeypatch_json_functions(monkeypatch):
+        monkeypatch.setattr('server.load_json', MockedJson.load_test_json)
+        monkeypatch.setattr('server.save_json', MockedJson.save_test_json)
 
 
 class TestJson:
 
-    mocked_json = MockedJson()
-
     def test_save_clubs(self, monkeypatch):
 
-        monkeypatch.setattr('server.load_json', self.mocked_json.load_json)  # >>
+        # Create a new clubs test file, then monkeypatch, then monkeypatch the json functions
+        MockedJson.generate_a_new_test_file('clubs', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
-        # Load the mocked list of clubs and add one
-        mocked_clubs = server.load_json(MockedJson.CLUBS)
+        # Load the mocked list of clubs and add a new one
+        mocked_clubs = server.load_json('clubs')
         mocked_clubs.append({"name": "Super_Club", "email": "none", "points": "555"})
 
-        # Save it (inside a test json file) and remove the monkeypatch
-        server.save_json(MockedJson.CLUBS, mocked_clubs)
-        monkeypatch.undo()  # <<
+        # Save it (inside a test json file)
+        server.save_json('clubs', mocked_clubs)
 
         # Open the test json file and check if the new club has been saved
-        clubs = server.load_json(MockedJson.CLUBS)
+        clubs = server.load_json('clubs')
 
         assert clubs[0]['name'] == "name_test_club"
         assert clubs[0]['points'] == "22"
@@ -58,15 +75,14 @@ class TestJson:
 
     def test_save_competitions(self, monkeypatch):
 
-        monkeypatch.setattr('server.load_json', self.mocked_json.load_json)  # >>
+        MockedJson.generate_a_new_test_file('competitions', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
-        mocked_comp = server.load_json(MockedJson.COMPETITIONS)
+        mocked_comp = server.load_json('competitions')
         mocked_comp.append({"name": "Super_Competition", "date": "1985-01-01 07:00:00", "numberOfPlaces": "5000"})
 
-        server.save_json(MockedJson.COMPETITIONS, mocked_comp)
-        monkeypatch.undo()  # <<
-
-        competitions = server.load_json(MockedJson.COMPETITIONS)
+        server.save_json('competitions', mocked_comp)
+        competitions = server.load_json('competitions')
 
         assert competitions[0]['name'] == "name_test_competition"
         assert competitions[0]['numberOfPlaces'] == "41"
@@ -76,17 +92,18 @@ class TestJson:
 
     def test_update_a_club(self, monkeypatch):
 
-        # Open the mocked json file and save it inside test_clubs.json
-        self.mocked_json.generate_a_new_test_file(MockedJson.CLUBS, monkeypatch)
+        # Generate a test file with the mocked club, then monkeypatch the json functions
+        MockedJson.generate_a_new_test_file('clubs', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
         # Take the first (and the only) entry and update it
-        club_to_update = server.load_json(MockedJson.CLUBS)[0]
+        club_to_update = server.load_json('clubs')[0]
         club_to_update['email'] = 'updated@mail.com'
         club_to_update['points'] = '10000'
-        server.update_json(MockedJson.CLUBS, club_to_update)
+        server.update_json('clubs', club_to_update)
 
         # Open the file and check if the update has been executed
-        updated_list_of_clubs = server.load_json(MockedJson.CLUBS)
+        updated_list_of_clubs = server.load_json('clubs')
 
         assert updated_list_of_clubs[0]['name'] == 'name_test_club'
         assert updated_list_of_clubs[0]['email'] == 'updated@mail.com'
@@ -94,14 +111,15 @@ class TestJson:
 
     def test_update_a_competition(self, monkeypatch):
 
-        self.mocked_json.generate_a_new_test_file(MockedJson.COMPETITIONS, monkeypatch)
+        MockedJson.generate_a_new_test_file('competitions', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
-        comp_to_update = server.load_json(MockedJson.COMPETITIONS)[0]
+        comp_to_update = server.load_json('competitions')[0]
         comp_to_update['date'] = '1990-02-02 05:05:05'
         comp_to_update['numberOfPlaces'] = '44'
-        server.update_json(MockedJson.COMPETITIONS, comp_to_update)
+        server.update_json('competitions', comp_to_update)
 
-        updated_list_of_comp = server.load_json(MockedJson.COMPETITIONS)
+        updated_list_of_comp = server.load_json('competitions')
 
         assert updated_list_of_comp[0]['name'] == 'name_test_competition'
         assert updated_list_of_comp[0]['date'] == '1990-02-02 05:05:05'
@@ -109,27 +127,29 @@ class TestJson:
 
     def test_add_a_club(self, monkeypatch):
 
-        # Open the mocked json file and save it inside test_clubs.json
-        self.mocked_json.generate_a_new_test_file(MockedJson.CLUBS, monkeypatch)
+        # Generate a test file with the mocked club, then monkeypatch the json functions
+        MockedJson.generate_a_new_test_file('clubs', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
         # Create and add a new club
         new_club = {'name': 'new_club', 'email': 'new@mail.com', 'points': '5'}
-        server.update_json(MockedJson.CLUBS, new_club)
+        server.update_json('clubs', new_club)
 
         # Open the file and check if the add has been executed
-        club = [c for c in server.load_json(MockedJson.CLUBS) if c['name'] == 'new_club'][0]
+        club = [c for c in server.load_json('clubs') if c['name'] == 'new_club'][0]
 
         assert club['email'] == 'new@mail.com'
         assert club['points'] == '5'
 
     def test_add_a_competition(self, monkeypatch):
 
-        self.mocked_json.generate_a_new_test_file(MockedJson.COMPETITIONS, monkeypatch)
+        MockedJson.generate_a_new_test_file('competitions', monkeypatch)
+        MockedJson.monkeypatch_json_functions(monkeypatch)
 
         new_competition = {'name': 'new_competition', 'date': '1970-06-06 04:04:00', 'numberOfPlaces': '88'}
-        server.update_json(MockedJson.COMPETITIONS, new_competition)
+        server.update_json('competitions', new_competition)
 
-        comp = [c for c in server.load_json(MockedJson.COMPETITIONS) if c['name'] == 'new_competition'][0]
+        comp = [c for c in server.load_json('competitions') if c['name'] == 'new_competition'][0]
 
         assert comp['date'] == '1970-06-06 04:04:00'
         assert comp['numberOfPlaces'] == '88'
