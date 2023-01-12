4d73f4d3bd537c33dd01dc2e8ca2ea901320d850
Cyl94700 <cyrilcouybes@gmail.com>
2023-01-12 10:10:38 +0100

    updating locustfile

--- tests/performance_tests/locustfile.py
+++ tests/performance_tests/locustfile.py
@@ -9,5 +9,20 @@ class LocustTestServer(HttpUser):
         self.client.post("/showSummary", data={'email': "john@simplylift.co"})
 
     @task
-    def task1(self):
-        pass
+    def booking_get(self):
+        self.client.get(f"/book/Spring Festival/Simply Lift")
+
+    @task
+    def booking_places_post(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 1,
+                "club": "Simply Lift",
+                "competition": "Spring Festival"
+                 }
+        )
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
