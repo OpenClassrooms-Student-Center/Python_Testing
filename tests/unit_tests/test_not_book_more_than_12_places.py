87243dd90e168bdc6bb8de92841d4e2e72df32a7
Cyl94700 <cyrilcouybes@gmail.com>
2023-01-12 15:00:39 +0100

    unit_tests: test_not_book_more_places_than_12_places

---
+++ tests/unit_tests/test_not_book_more_than_12_places.py
@@ -0,0 +1,73 @@
+import server
+from server import app
+
+
+class TestNotMoreThanTwelvePoints:
+
+    client = app.test_client()
+    competition = [
+        {
+            "name": "Test",
+            "date": "2023-03-27 10:00:00",
+            "numberOfPlaces": "40"
+        }
+    ]
+
+    club = [
+        {
+            "name": "Test_club",
+            "email": "test_club@email.com",
+            "points": "25"
+        }
+    ]
+
+    booked_places = [
+        {
+            "competition": "Test",
+            "booked": [5, "Test_club"]
+        }
+    ]
+
+    def setup_method(self):
+        server.competitions = self.competition
+        server.clubs = self.club
+        server.booked_places = self.booked_places
+
+    def test_less_than_or_equal_twelve(self):
+
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 7,
+                "club": self.club[0]["name"],
+                "competition": self.competition[0]["name"]
+            }
+        )
+
+        assert result.status_code == 200
+
+    def test_more_than_twelve_at_once(self):
+
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 13,
+                "club": self.club[0]["name"],
+                "competition": self.competition[0]["name"]
+            }
+        )
+
+        assert result.status_code == 403
+
+    def test_more_than_twelve_in_several_time(self):
+
+        result = self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 1,
+                "club": self.club[0]["name"],
+                "competition": self.competition[0]["name"]
+            }
+        )
+
+        assert result.status_code == 403
