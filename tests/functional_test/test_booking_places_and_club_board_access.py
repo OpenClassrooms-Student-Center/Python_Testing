a79574f0a75902e83b221887081aa930f2341d36
Cyl94700 <cyrilcouybes@gmail.com>
2023-01-22 14:45:02 +0100

    functional test with headless option

--- tests/functional_test/test_booking_places_and_club_board_access.py
+++ tests/functional_test/test_booking_places_and_club_board_access.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('disable-infobars')
