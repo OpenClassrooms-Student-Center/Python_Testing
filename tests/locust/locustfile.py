05c362fc6e03fcb92adf363a4da0dff3ebeeffe6
Yann BRONNER <yann.bronner@gmail.com>
2023-01-23 10:47:33 +0100

    Add more tests setup

---
+++ tests/locust/locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpUser, task
+
+
+class HelloWorldUser(HttpUser):
+    @task
+    def hello_world(self):
+        self.client.get("/")
+        payload = {
+                'email': 'kate@shelifts.co.uk',
+                }
+        self.client.post('/showSummary', data=payload)
