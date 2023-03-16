c7e050f00dbd56c27c94a4da33e283f5794809d0
Remy Gosselin <gosselin.remy@gmail.com>
2023-03-16 16:13:47 +0400

    cap max value to 12

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,79 +10,78 @@ from ..server import create_app, loadClubs
 def pytest_configure(config):
     logging.basicConfig(level=logging.INFO)
 
-@pytest.fixture
-def client():
-    app = create_app({"TESTING": True})
-
-    with app.test_client() as client:
-        yield client
-
-@pytest.fixture
-def purchaseRequest():
-    purchase = {
-		'club': 'Simply Lift',
-		'competition': 'Fall Classic',
-		'places': '4'
-		}
-    
-    yield purchase
-
-@pytest.fixture
-def purchaseMorePointThanCompetition():
-    purchase = {
-		'club': 'Simply Lift',
-		'competition': 'Fall Classic',
-		'places': '14'
-		}
-    
-    yield purchase
-
-@pytest.fixture
-def purchaseMorePointThanClub():
-    purchase = {
-		'club': 'Iron Temple',
-		'competition': 'Fall Classic',
-		'places': '5'
-		}
-    
-    yield purchase
-
-@pytest.fixture
-def purchaseNoPointSpecified():
-    purchase = {
-		'club': 'Iron Temple',
-		'competition': 'Fall Classic',
-		'places': ''
-		}
-    
-    yield purchase
-
-@pytest.fixture
-def patch_clubs(monkeypatch):
-
-    clubs_list = clubs_dataset()
-    club_json = json.dumps(clubs_list)
-
-    monkeypatch.setenv('server.clubs', club_json)
-    yield club_json
-    monkeypatch.delenv('server.clubs')
-
-@pytest.fixture
-def patch_competitions(monkeypatch):
-
-    competitions_list = competitions_dataset()
-    competitions_json = json.dumps(competitions_list)
-
-    monkeypatch.setenv('server.competitions', competitions_json)
-    yield competitions_json
-    monkeypatch.delenv('server.competitions')
-
 def clubs_dataset():
     with open('data/clubs_test.json') as c:
          listOfClubs = json.load(c)['clubs']
     return listOfClubs
 
 def competitions_dataset():
-    with open('data/competitions_tests.json') as c:
-         listOfCompetitions = json.load(c)['clubs']
+    with open('data/competitions_test.json') as c:
+         listOfCompetitions = json.load(c)['competitions']
     return listOfCompetitions
+
+@pytest.fixture
+def client():
+    app = create_app({"TESTING": True})
+
+    clubs = clubs_dataset()
+    competitions = competitions_dataset()
+
+    os.environ['server.clubs'] = json.dumps(clubs)
+    os.environ['server.competitions'] = json.dumps(competitions)
+
+    with app.test_client() as client:
+        yield client
+    
+    del os.environ['server.clubs']
+    del os.environ['server.competitions']
+
+@pytest.fixture
+def purchaseBase():
+    purchase = {
+		'club': 'club test base',
+		'competition': 'Competition Test base',
+		'places': '4'
+		}
+    
+    yield purchase
+
+@pytest.fixture
+def purchase12points():
+    purchase = {
+		'club': 'club test more than 12 points',
+		'competition': 'Competition Test 12 points',
+		'places': '13'
+		}
+    
+    yield purchase
+
+@pytest.fixture
+def purchaseEmpty():
+    purchase = {
+		'club': 'club test base',
+		'competition': 'Competition Test base',
+		'places': ''
+		}
+    
+    yield purchase
+
+@pytest.fixture
+def purchaseNotEnoughPointsCompetition():
+    purchase = {
+		'club': 'club test base',
+		'competition': 'Competition Test not enough points',
+		'places': '4'
+		}
+    
+    yield purchase
+
+@pytest.fixture
+def purchaseNotEnoughPointsClub():
+    purchase = {
+		'club': 'club test not enough points',
+		'competition': 'Competition Test base',
+		'places': '4'
+		}
+    
+    yield purchase
