53ca5f35e379d6bc4e86ee1a7f4aab36b768db00
Remy Gosselin <gosselin.remy@gmail.com>
2023-03-18 17:09:35 +0400

    refacto

--- server.py
+++ server.py
@@ -79,11 +79,6 @@ def create_app(config):
             return render_template('welcome.html', club=club, competitions=competitions)
         
         else:
-            # update competition points
-            competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-            # update club points
-            club['points'] = str(int(club['points']) - placesRequired)
-
             flash('Great-booking complete!')
             return render_template('welcome.html', club=club, competitions=competitions)
 
