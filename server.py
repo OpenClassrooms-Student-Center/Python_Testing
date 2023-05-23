b53d16432d2cc9937052f5c223791ea56d7fe63d
dastfox <theo.renard.parne@gmail.com>
2023-05-23 11:17:59 +0200

    feat(clubs): check that points are reflected

--- server.py
+++ server.py
@@ -1,78 +1,103 @@
 import datetime
 import json
-from flask import Flask,render_template,request,redirect,flash,url_for
+from flask import Flask, render_template, request, redirect, flash, url_for
 
 
 def loadClubs():
-    with open('clubs.json') as c:
-         listOfClubs = json.load(c)['clubs']
-         return listOfClubs
+    with open("clubs.json") as c:
+        listOfClubs = json.load(c)["clubs"]
+        return listOfClubs
 
 
 def loadCompetitions():
-    with open('competitions.json') as comps:
-         listOfCompetitions = json.load(comps)['competitions']
-         return listOfCompetitions
+    with open("competitions.json") as comps:
+        listOfCompetitions = json.load(comps)["competitions"]
+        return listOfCompetitions
 
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = "something_special"
 
 competitions = loadCompetitions()
 clubs = loadClubs()
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/showSummary', methods=['POST'])
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/showSummary", methods=["POST"])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']]
+    club = [club for club in clubs if club["email"] == request.form["email"]]
     if len(club) == 0:
         message = "Email not found. Please try again."
-        return render_template('index.html', message=message)
+        return render_template("index.html", message=message)
     else:
-        return render_template('welcome.html', club=club[0], competitions=competitions, today=str(datetime.date.today()))
+        return render_template(
+            "welcome.html",
+            club=club[0],
+            competitions=competitions,
+            today=str(datetime.date.today()),
+        )
 
 
-
-@app.route('/book/<competition>/<club>')
-def book(competition,club):
-    foundClub = [c for c in clubs if c['name'] == club][0]
-    foundCompetition = [c for c in competitions if c['name'] == competition][0]
+@app.route("/book/<competition>/<club>")
+def book(competition, club):
+    foundClub = [c for c in clubs if c["name"] == club][0]
+    foundCompetition = [c for c in competitions if c["name"] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template(
+            "booking.html", club=foundClub, competition=foundCompetition
+        )
     else:
         flash("Something went wrong-please try again")
-        return render_template('welcome.html', club=club, competitions=competitions)
+        return render_template("welcome.html", club=club, competitions=competitions)
 
 
-
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
+    competition_name = request.form["competition"]
+    club_name = request.form["club"]
+    places_required = int(request.form["places"])
+
+    competition = next(c for c in competitions if c["name"] == competition_name)
+    club = next(c for c in clubs if c["name"] == club_name)
 
     # Check if club has enough points
-    if int(club['points']) < placesRequired:
-        flash('Cannot redeem more points than available!')
-    elif int(competition['numberOfPlaces']) < placesRequired:
-        flash('Cannot book more places than available!')
-    elif placesRequired > 12:
-        flash('Cannot redeem more than 12 places!')
+    if int(club["points"]) < places_required:
+        flash("Cannot redeem more points than available!")
+    elif int(competition["numberOfPlaces"]) < places_required:
+        flash("Cannot book more places than available!")
+    elif places_required > 12:
+        flash("Cannot redeem more than 12 places!")
     else:
-        competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
-        club['points'] = int(club['points']) - placesRequired
-        flash('Great-booking complete!')
+        # Update the competition's available places and club's points
+        competition["numberOfPlaces"] = str(
+            int(competition["numberOfPlaces"]) - places_required
+        )
+        club["points"] = str(int(club["points"]) - places_required)
+        flash("Great-booking complete!")
 
-    return render_template('welcome.html', club=club, competitions=competitions)
+        # Save the updated data back to the JSON files
+        saveClubs()
+        saveCompetitions()
+
+    return render_template("welcome.html", club=club, competitions=competitions)
 
 
+def saveClubs():
+    data = {"clubs": clubs}
+    with open("clubs.json", "w") as file:
+        json.dump(data, file)
 
-# TODO: Add route for points display
+
+def saveCompetitions():
+    data = {"competitions": competitions}
+    with open("competitions.json", "w") as file:
+        json.dump(data, file)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
--- server_test.py
+++ server_test.py
@@ -1,22 +1,25 @@
 import pytest
+
 # from server import app
 from server import *
 
+
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
 
 
 def test_showSummary(client):
-    response = client.post('/showSummary', data=dict(email='john@simplylift.co'))
-    assert b'<title>Summary | GUDLFT Registration</title>' in response.data
+    response = client.post("/showSummary", data=dict(email="john@simplylift.co"))
+    assert b"<title>Summary | GUDLFT Registration</title>" in response.data
 
 
 def test_showSummary_unknown_email(client):
-    response = client.post('/showSummary', data=dict(email='test@example.com'))
-    assert b'<title>GUDLFT Registration</title>' in response.data
+    response = client.post("/showSummary", data=dict(email="test@example.com"))
+    assert b"<title>GUDLFT Registration</title>" in response.data
+
 
 def test_purchase_places_exceeding_points(client):
     # load the clubs and competitions data
@@ -24,54 +27,102 @@ def test_purchase_places_exceeding_points(client):
     competitions = loadCompetitions()
 
     # find a club and a competition for testing
-    club = next(c for c in clubs if c['name'] == 'Simply Lift')
-    competition = next(c for c in competitions if c['name'] == 'Spring Festival')
+    club = next(c for c in clubs if c["name"] == "Simply Lift")
+    competition = next(c for c in competitions if c["name"] == "Spring Festival")
 
     # count the initial number of points
-    initial_points = int(club['points'])
+    initial_points = int(club["points"])
 
     # try to purchase more places than the club has points
-    places_required = initial_points + 10
+    places_required = initial_points + 1
     response = client.post(
-        '/purchasePlaces', 
+        "/purchasePlaces",
         data=dict(
-            competition=competition['name'],
-            club=club['name'],
-            places=places_required
-        ), 
-        follow_redirects=True
+            competition=competition["name"],
+            club=club["name"],
+            places=places_required,
+        ),
+        follow_redirects=True,
     )
 
     # response should indicate that the purchase was not possible
-    assert b'Cannot redeem more points than available' in response.data
+    assert b"Cannot redeem more points than available" in response.data
+    # TODO club's points should not have changed
 
-    
+
 def test_purchase_places_max_points(client):
     # load the clubs and competitions data
     clubs = loadClubs()
     competitions = loadCompetitions()
 
     # find a club and a competition for testing
-    club = next(c for c in clubs if c['name'] == 'Simply Lift')
-    competition = next(c for c in competitions if c['name'] == 'Spring Festival')
+    club = next(c for c in clubs if c["name"] == "Simply Lift")
+    competition = next(c for c in competitions if c["name"] == "Spring Festival")
 
     # count the initial number of points
-    initial_points = int(club['points'])
+    initial_points = int(club["points"])
+    if initial_points >= 13:
+        # try to purchase more places than 12
+        places_required = 13
+        response = client.post(
+            "/purchasePlaces",
+            data=dict(
+                competition=competition["name"],
+                club=club["name"],
+                places=places_required,
+            ),
+            follow_redirects=True,
+        )
 
-    # try to purchase more places than 12
-    places_required = 13
+        # response should indicate that the purchase was not possible
+
+        assert b"Cannot redeem more than 12 places!" in response.data
+
+
+def test_purchase_places_deducts_points(client):
+    # Load the clubs and competitions data
+    clubs = loadClubs()
+    competitions = loadCompetitions()
+
+    # Find a club and a competition for testing
+    club = next(c for c in clubs if c["name"] == "Simply Lift")
+    competition = next(c for c in competitions if c["name"] == "Spring Festival")
+
+    # Save the initial number of points and available places
+    initial_points = int(club["points"])
+    initial_places = int(competition["numberOfPlaces"])
+
+    # Define the number of places to be purchased
+    places_required = 5
+
+    # Make a POST request to purchase places
     response = client.post(
-        '/purchasePlaces', 
+        "/purchasePlaces",
         data=dict(
-            competition=competition['name'],
-            club=club['name'],
-            places=places_required
-        ), 
-        follow_redirects=True
+            competition=competition["name"],
+            club=club["name"],
+            places=places_required,
+        ),
+        follow_redirects=True,
     )
 
-    # response should indicate that the purchase was not possible
-    assert b'Cannot redeem more than 12 places!' in response.data
+    # Assert that the response indicates a successful purchase
+    if initial_points >= places_required:
+        assert b"Great-booking complete!" in response.data
+        clubs = loadClubs()
+        competitions = loadCompetitions()
 
-    # club's points should not have changed
-    assert int(club['points']) == initial_points
+        # Retrieve the updated club and competition data
+        updated_club = next(c for c in clubs if c["name"] == "Simply Lift")
+        updated_competition = next(
+            c for c in competitions if c["name"] == "Spring Festival"
+        )
+
+        # Assert that the club's points have been deducted
+        assert int(updated_club["points"]) == initial_points - places_required
+
+        # Assert that the competition's available places have been updated
+        assert (
+            int(updated_competition["numberOfPlaces"])
+            == initial_places - places_required
+        )
