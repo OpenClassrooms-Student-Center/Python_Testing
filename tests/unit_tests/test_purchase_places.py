2bf653b676971313f40daea9591302ecb3be8dfe
Nicolas Deleu <n.deleu@hotmail.fr>
2023-07-12 06:08:08 +0200

    fix issue 04

--- tests/unit_tests/test_purchase_places.py
+++ tests/unit_tests/test_purchase_places.py
@@ -34,3 +34,15 @@ def test_purchase_places_post_not_enough_places_400(client, existent_club, exist
     assert rv.status_code == 400
     data = rv.data.decode()
     assert "Not enough places available for this purchase" in data
+
+
+def test_purchase_places_post_more_than_12_places_400(client, existent_club, existent_competition):
+    # If the secretary try to buy more than 12 places from the same competition
+    rv = client.post("/purchasePlaces",
+                     data={"club": existent_club[0]["name"],
+                           "competition": existent_competition[0]["name"],
+                           "places": 13
+                           })
+    assert rv.status_code == 400
+    data = rv.data.decode()
+    assert "You cannot buy more than 12 tickets from the same competition" in data
