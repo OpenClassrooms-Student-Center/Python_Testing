a020a7a2ba3060e70b5c198ec2bd5fed880e01a5
Nicolas Deleu <n.deleu@hotmail.fr>
2023-07-13 02:05:12 +0200

    finish issue 06

--- tests/unit_tests/test_log.py
+++ tests/unit_tests/test_log.py
@@ -2,13 +2,13 @@ from tests.config import client, existent_club, non_existent_club
 
 
 def test_get_login_page_200(client):
-
+    # Success access to login page
     rv = client.get('/')
     assert rv.status_code == 200
 
 
 def test_login_post_email_200(client, existent_club):
-
+    # Try and success log with good email
     rv = client.post("/showSummary",
                      data={"email": existent_club[2]["email"]},
                      follow_redirects=True)
@@ -18,7 +18,7 @@ def test_login_post_email_200(client, existent_club):
 
 
 def test_login_post_email_401(client, non_existent_club):
-
+    # Try and fail log with wrong email
     rv = client.post("/showSummary",
                      data={"email": non_existent_club[0]["email"]},
                      follow_redirects=True)
@@ -28,5 +28,6 @@ def test_login_post_email_401(client, non_existent_club):
 
 
 def test_logout_user(client):
+    # Success Logout
     rv = client.get("/logout")
     assert rv.status_code == 302
