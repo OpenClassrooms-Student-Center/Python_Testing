e60d6981c2b16cf5778517f644044e7a7c583367
Archy <antoine.tassez@gmail.com>
2023-08-23 14:03:42 +0200

    Changement de nom de la fonction et amélioration de la gestion des erreurs dans la route show_summary

--- server.py
+++ server.py
@@ -25,9 +25,14 @@ def index():
     return render_template('index.html')
 
 @app.route('/showSummary',methods=['POST'])
-def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+def show_summary():
+    email = request.form['email']
+    club = next((club for club in clubs if club['email'] == email), False)
+    if club:
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+        flash("Sorry, that email wasn't found.")
+        return redirect(url_for('index'))
 
 
 @app.route('/book/<competition>/<club>')
