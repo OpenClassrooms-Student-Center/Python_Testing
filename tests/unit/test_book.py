9f4f2a3117d07a03af24fbc5b3586a759b0453f8
Archy <antoine.tassez@gmail.com>
2023-08-31 15:51:00 +0200

    Ajout de tests sur la fonction book

--- tests/unit/test_book.py
+++ tests/unit/test_book.py
@@ -1,5 +1,6 @@
 from tests.tests_utils import decode_response
 
+
 def test_book_competition_over(app):
     client = app.test_client()
     response = client.get(
@@ -24,7 +25,28 @@ def test_book_competition_going(app):
 
     decoded_response = decode_response(response.data)
 
-    assert (
-        f"This competition is open until"
-        in decoded_response
+    assert f"This competition is open until" in decoded_response
+
+
+def test_book_non_existing_competition(app):
+    client = app.test_client()
+    response = client.get(
+        f"/book/DoesntExist/book",
+        follow_redirects=True,
     )
+
+    decoded_response = decode_response(response.data)
+
+    assert "Something went wrong-please try again" in decoded_response
+
+
+def test_book_non_existing_club(app):
+    client = app.test_client()
+    response = client.get(
+        f"/book/empty/DoesntExist",
+        follow_redirects=True,
+    )
+
+    decoded_response = decode_response(response.data)
+
+    assert "Something went wrong-please try again" in decoded_response
