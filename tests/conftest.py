70fdc95d0a2fe3af29ba2458fe72b83d259c3ff2
Archy <antoine.tassez@gmail.com>
2023-08-31 13:50:29 +0200

    Séparation de la 'bdd' de prod et celle des tests ;

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
 import server
+
+server.clubs = server.load_clubs('tests/clubs.json')
+server.competitions = server.load_competitions('tests/competitions.json')
+
 @pytest.fixture
 def app():
     app = server.app
