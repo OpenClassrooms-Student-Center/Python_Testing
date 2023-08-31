5b1243ade27e3d5ae4249d0a2b769be7bac3c59f
Archy <antoine.tassez@gmail.com>
2023-08-31 15:49:41 +0200

    Ajout de tests sur la fonction show_summary

--- tests/unit/test_show_summary.py
+++ tests/unit/test_show_summary.py
@@ -10,3 +10,30 @@ def test_invalid_email(app):
     decoded_response = decode_response(response.data)
 
     assert "Sorry, that email wasn't found." in decoded_response
+
+
+def test_no_email(app):
+    client = app.test_client()
+    response = client.post("/showSummary", data={}, follow_redirects=True)
+
+    decoded_response = decode_response(response.data)
+
+    assert "Sorry, that email wasn't found." in decoded_response
+
+
+def test_email_valid(app):
+    client = app.test_client()
+    response = client.post(
+        "/showSummary", data={"email": "book@test.srv"}, follow_redirects=True
+    )
+
+    decoded_response = decode_response(response.data)
+    assert "Welcome, book@test.srv" in decoded_response
+
+
+def test_logout(app):
+    client = app.test_client()
+    response = client.get("/logout", follow_redirects=True)
+
+    decoded_response = decode_response(response.data)
+    assert "Welcome to the GUDLFT Registration Portal!" in decoded_response
