7926b64c922a1facd2426da7ce5b7800dc65a964
Archy <antoine.tassez@gmail.com>
2023-08-31 19:56:27 +0200

    Ajout des statuts Https des réponses du serveur

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 from datetime import datetime
+from http import HTTPStatus
 
 from flask import Flask, render_template, request, redirect, flash, url_for
 
@@ -34,7 +35,7 @@ competitions = load_competitions("competitions.json")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html"), HTTPStatus.OK
 
 
 @app.route("/showSummary", methods=["POST"])
@@ -42,10 +43,13 @@ def show_summary():
     email = request.form.get("email", None)
     club = next((club for club in clubs if club["email"] == email), False)
     if club:
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.OK,
+        )
     else:
         flash("Sorry, that email wasn't found.")
-        return redirect(url_for("index"))
+        return render_template("index.html"), HTTPStatus.BAD_REQUEST
 
 
 @app.route("/book/<competition>/<club>")
@@ -65,17 +69,26 @@ def book(competition, club):
 
     if found_competition and is_competition_over(found_competition):
         flash("Sorry, this competition is over, places are not available anymore.")
-        return render_template("welcome.html", club=found_club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=found_club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
     if found_club and found_competition:
-        return render_template(
-            "booking.html",
-            club=found_club,
-            competition=found_competition,
+        return (
+            render_template(
+                "booking.html",
+                club=found_club,
+                competition=found_competition,
+            ),
+            HTTPStatus.OK,
         )
     else:
         flash("Something went wrong-please try again")
-        return render_template("welcome.html", club=found_club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=found_club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
 
 @app.route("/purchasePlaces", methods=["POST"])
@@ -85,17 +98,26 @@ def purchase_places():
 
     if not club:
         flash("Something went wrong-please try again")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
-    placesRequired = request.form.get("places", "%")
-    if not placesRequired.isdigit():
+    places_required = request.form.get("places", "%")
+    if not places_required.isdigit():
         flash("Please provide a valid rounded number")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
-    placesRequired = int(placesRequired)
-    if placesRequired > 12:
+    places_required = int(places_required)
+    if places_required > 12:
         flash("Sorry you can't book more than 12 places")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
     competition_name = request.form.get("competition", False)
 
@@ -110,25 +132,37 @@ def purchase_places():
 
     if not competition or is_competition_over(competition):
         flash("Something went wrong-please try again")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
     competition_remaining_places = int(competition.get("numberOfPlaces", 0))
 
     if not competition_remaining_places > 0:
         flash("Sorry this competition is already full.")
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
-    club_remaining_point = int(club["points"]) - placesRequired
+    club_remaining_point = int(club["points"]) - places_required
     if club_remaining_point < 0:
         flash(
-            f"Sorry you can't book more than {club_remaining_point + placesRequired} places."
+            f"Sorry you can't book more than {club_remaining_point + places_required} places."
         )
-        return render_template("welcome.html", club=club, competitions=competitions)
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            HTTPStatus.BAD_REQUEST,
+        )
 
     club["points"] = club_remaining_point
-    competition["numberOfPlaces"] = competition_remaining_places - placesRequired
+    competition["numberOfPlaces"] = competition_remaining_places - places_required
     flash("Great-booking complete!")
-    return render_template("welcome.html", club=club, competitions=competitions)
+    return (
+        render_template("welcome.html", club=club, competitions=competitions),
+        HTTPStatus.OK,
+    )
 
 
 # TODO: Add route for points display
@@ -136,4 +170,4 @@ def purchase_places():
 
 @app.route("/logout")
 def logout():
-    return redirect(url_for("index"))
+    return redirect(url_for("index"), HTTPStatus.FOUND)
