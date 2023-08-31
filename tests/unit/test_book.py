c28b9d87fa31060424d4324242a0cee24606c6dd
Archy <antoine.tassez@gmail.com>
2023-08-31 14:03:40 +0200

    Harmonisation de la 'bdd' de test et des fichiers de tests

--- tests/unit/test_book.py
+++ tests/unit/test_book.py
@@ -1,24 +1,9 @@
 from tests.tests_utils import decode_response
 
-TEST_CLUB = {"name": "Test club", "email": "admin@admin.com", "points": "5"}
-
-TEST_COMPETITION_OVER = {
-    "name": "Test competition",
-    "date": "2020-03-27 10:00:00",
-    "numberOfPlaces": "10",
-}
-
-TEST_COMPETITION_GOING = {
-    "name": "Test competition going",
-    "date": "2026-03-27 10:00:00",
-    "numberOfPlaces": "10",
-}
-
-
 def test_book_competition_over(app):
     client = app.test_client()
     response = client.get(
-        f"/book/{TEST_COMPETITION_OVER['name']}/{TEST_CLUB['name']}",
+        f"/book/over/book",
         follow_redirects=True,
     )
 
@@ -33,13 +18,13 @@ def test_book_competition_over(app):
 def test_book_competition_going(app):
     client = app.test_client()
     response = client.get(
-        f"/book/{TEST_COMPETITION_GOING['name']}/{TEST_CLUB['name']}",
+        f"/book/going/book",
         follow_redirects=True,
     )
 
     decoded_response = decode_response(response.data)
 
     assert (
-        f"This competition is open until {TEST_COMPETITION_GOING['date']}."
+        f"This competition is open until"
         in decoded_response
     )
--- tests/unit/test_purchase_places.py
+++ tests/unit/test_purchase_places.py
@@ -1,32 +1,25 @@
 from tests.tests_utils import decode_response
 
-TEST_CLUB = {"name": "Test club", "email": "admin@admin.com", "points": "5"}
-
-TEST_COMPETITION = {
-    "name": "Test competition",
-    "date": "2020-03-27 10:00:00",
-    "numberOfPlaces": "10",
-}
 
 def test_not_enough_points(app):
     client = app.test_client()
     response = client.post(
         "/purchasePlaces",
-        data={"competition": "Test competition", "club": "Test club", "places": "10"},
+        data={"competition": "empty", "club": "purchase_places", "places": "11"},
         follow_redirects=True,
     )
 
     decoded_response = decode_response(response.data)
-    assert "Sorry you can't book more than 5 places." in decoded_response
+    assert "Sorry you can't book more than 10 places." in decoded_response
 
 
 def test_points_are_deducted_after_booking(app):
     client = app.test_client()
     response = client.post(
         "/purchasePlaces",
-        data={"competition": "Test competition", "club": "Test club", "places": "3"},
+        data={"competition": "empty", "club": "purchase_places", "places": "1"},
         follow_redirects=True,
     )
 
     decoded_response = decode_response(response.data)
-    assert "Points available: 2" in decoded_response
+    assert "Points available: 9" in decoded_response
