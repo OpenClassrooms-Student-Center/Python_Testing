e5101367de2173ae74f8e9edbb6bb2e95002a7ec
Archy <antoine.tassez@gmail.com>
2023-09-08 13:43:27 +0200

    Mise à jour des test locust pour prendre en compte le nouveau template ; Ajout d'un contrôle au cas où l'utilisateur utilise la totalité de ses points lors du load test de purchasePlaces

--- locustfile.py
+++ locustfile.py
@@ -14,11 +14,22 @@ class LocustPerfTest(HttpUser):
     def book(self):
         self.client.get("book/Spring Festival/Simply Lift")
 
+    @task
     def purchasePlaces(self):
-        self.client.post(
-            "/purchasePlaces",
+        with self.client.post(
+            "purchasePlaces",
             data={"competition": "Spring Festival", "club": "Simply Lift", "places": 1},
-        )
+            catch_response=True,
+        ) as response:
+            if (
+                response.status_code == 400
+                and "Sorry you dont have anymore points." in response.text
+            ):
+                response.success()
+
+    @task
+    def displayBoard(self):
+        self.client.get("displayBoard")
 
     @task
     def logout(self):
