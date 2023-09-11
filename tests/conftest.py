0eed48d17f7eb5dc4affc5b4c87c6bfea7be70ae
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-09-11 15:19:26 +0200

    Tests réussis pour le bug #2

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,20 +11,58 @@ def client():
 @pytest.fixture
 def clubs():
     clubs = [
-            {
-                "name":"Simply Lift",
-                "email":"john@simplylift.co",
-                "points":"13"
-            },
-            {
-                "name":"Iron Temple",
-                "email": "admin@irontemple.com",
-                "points":"4"
-            },
-            {   "name":"She Lifts",
-                "email": "kate@shelifts.co.uk",
-                "points":"12"
-            }
-        ]
+        {
+            "name":"Simply Lift",
+            "email":"john@simplylift.co",
+            "points":"13"
+        },
+        {
+            "name":"Iron Temple",
+            "email": "admin@irontemple.com",
+            "points":"4"
+        },
+        {   "name":"She Lifts",
+            "email": "kate@shelifts.co.uk",
+            "points":"12"
+        }
+    ]
     
     return clubs
+
+@pytest.fixture
+def single_club():
+    club = {
+        "name":"Simply Lift",
+        "email":"john@simplylift.co",
+        "points":"13"
+    }
+    
+    return club
+
+@pytest.fixture
+def single_competition():
+    competition = {
+        "name": "Spring Festival",
+        "date": "2020-03-27 10:00:00",
+        "numberOfPlaces": "25"
+    }
+    
+    return competition
+
+
+@pytest.fixture
+def competitions():
+    competitions = [
+        {
+            "name": "Spring Festival",
+            "date": "2020-03-27 10:00:00",
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Fall Classic",
+            "date": "2020-10-22 13:30:00",
+            "numberOfPlaces": "13"
+        }
+    ]
+    
+    return competitions
