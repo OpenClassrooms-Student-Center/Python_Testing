6c5cab8f25ee394932420281c89935c038cf8e91
Ciran GÜRBÜZ <ciran.gurbuz@gmail.com>
2023-09-12 17:16:23 +0200

    Correction to bug resolution Nr 1

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -1,17 +1,22 @@
 import pytest
 from server import app
 from tests.conftest import client, clubs, single_club, single_competition, competitions
+import server
+from server import app, competitions, clubs
 
 
 class TestShowSummary:   
-    def test_show_summary_with_existing_email(self, client, clubs):
-        app.config["clubs"] = clubs
+    def test_show_summary_with_existing_email(self, client, clubs, competitions, monkeypatch):
+        monkeypatch.setattr(server, "competitions", competitions)
+        monkeypatch.setattr(server, "clubs", clubs)
+        
         response = client.post("/showSummary", data={"email": "john@simplylift.co"})
         
         assert b"john@simplylift.co" in response.data
         
-    def test_login_with_invalid_email(self, client, clubs):
-        app.config["clubs"] = clubs
+    def test_login_with_invalid_email(self, client, clubs, competitions, monkeypatch):
+        monkeypatch.setattr(server, "competitions", competitions)
+        monkeypatch.setattr(server, "clubs", clubs)
         response = client.post("/showSummary", data={"email": "wrong@email.com"})
         
         assert b"Email not found. Please try a valid email." in response.data
