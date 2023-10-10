479af520ff9fa12ecd2a3b990ca5bd41fffb5c79
Bubhux <bubhuxpaindepice@gmail.com>
2023-10-10 16:24:15 +0200

    FIX BUG Modification et correction de la fonction def purchasePlaces

--- server.py
+++ server.py
@@ -54,14 +54,49 @@ def book(competition,club):
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
-    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
-    club = [c for c in clubs if c['name'] == request.form['club']][0]
-    placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
-    return render_template('welcome.html', club=club, competitions=competitions)
+    try:
+        competition_name = request.form['competition']
+        club_name = request.form['club']
+        places_input = request.form['places']
+
+        # Rechercher la compétition et le club dans la liste des compétitions et des clubs
+        competition = next((c for c in competitions if c['name'] == competition_name), None)
+        club = next((c for c in clubs if c['name'] == club_name), None)
+
+        placesRequired = int(places_input)
+
+        # Vérifier si l'utilisateur a suffisamment de points (maximum 12 athlètes)
+        if placesRequired > 12:
+            raise MaximumPlacesException()
+
+        # Convertir club['points'] et competition['numberOfPlaces'] en entiers
+        club_points = int(club['points']) if club['points'] else 0
+        competition_places = int(competition['numberOfPlaces']) if competition['numberOfPlaces'] else 0
+
+        # Vérifier si l'utilisateur a suffisamment de points (1 point par inscription)
+        if club_points < placesRequired:
+            raise NotEnoughPointsException()
+
+        # Effectuer la réservation
+        competition_places -= placesRequired
+        club_points -= placesRequired
+
+        # Mettre à jour les valeurs dans les dictionnaires
+        competition['numberOfPlaces'] = str(competition_places)
+        club['points'] = str(club_points)
+
+        flash('Great-booking complete!')
+        return render_template('welcome.html', club=club, competitions=competitions)
+
+    except MaximumPlacesException:
+        flash(MaximumPlacesException.flash_message, 'error')
+        return redirect(url_for('book', competition=competition_name, club=club_name))
+    
+    except NotEnoughPointsException:
+        flash(NotEnoughPointsException.flash_message, 'error')
+        return redirect(url_for('book', competition=competition_name, club=club_name))
 
 
 # TODO: Add route for points display
