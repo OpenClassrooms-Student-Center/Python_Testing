e2a08f3298dfc9d6f779599aba0d7c759e65bfcb
AlexBotswana <alfacama@outlook.fr>
2024-02-21 22:43:42 +0200

    Bug competition past
    Issue #5

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
+from datetime import datetime
 
 
 def loadClubs():
@@ -37,12 +38,16 @@ def showSummary():
 def book(competition,club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+    dateCompetition = datetime.strptime(foundCompetition['date'], "%Y-%m-%d %H:%M:%S")
+    if dateCompetition >= datetime.now():
+        if foundClub and foundCompetition:
+            return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        else:
+            flash("Something went wrong-please try again")
+            return render_template('welcome.html', club=club, competitions=competitions)
     else:
-        flash("Something went wrong-please try again")
+        flash('Competition is over. You can not buy places')
         return render_template('welcome.html', club=club, competitions=competitions)
-
 
 
 @app.route('/purchasePlaces',methods=['POST'])
