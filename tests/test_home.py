22d7d13ae691999495e0434895b9a5451fdfc829
AurelienAllenic <aurelien.allenic@gmail.com>
2024-03-14 18:26:58 +0100

    (fix) deleted useless login and home.py

--- tests/test_home.py
+++
@@ -1,7 +0,0 @@
-def test_index(test_client):
-
-    """Test the index route."""
-
-    response = test_client.get("/")
-    assert response.status_code == 200
-    assert b"Welcome to the GUDLFT Registration Portal!" in response.data
